7254bbe663e9bf96c363fcecb0db6b9dd46a4116
Jesse Lloyd <116366380+jlloyd0430@users.noreply.github.com>
2023-04-29 17:06:18 -0400

    Update tinymce.js

--- public/js/tinymce.js
+++ public/js/tinymce.js
@@ -1,6 +1,6 @@
 tinymce.init({
   selector: "#serviceDescription",
-  apiKey: "68ig7ixv8x5wcfkmmv2fod17zy042hl4qba8p5ngvavyjie5",
+  apiKey: TINY_CLOUD_API_KEY,
   plugins: [
     "advlist lists link autolink autosave code",
     "preview",
